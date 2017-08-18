1bd468a634153e9e700f3bc45e5a79a4ee3ae62f
DX37 <asusx2@mail.ru>
2017-08-18 15:21:59 +0700

    List unlisted files
    Change-Id: I2e0f7168f2c594bcc391c24df39e93e45da0f0d9

--- device.mk
+++ device.mk
@@ -54,7 +54,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/acdb/QRD_Hdmi_cal.acdb:system/etc/acdbdata/QRD/QRD_Hdmi_cal.acdb \
     $(LOCAL_PATH)/audio/acdb/QRD_Headset_cal.acdb:system/etc/acdbdata/QRD/QRD_Headset_cal.acdb \
     $(LOCAL_PATH)/audio/acdb/QRD_Speaker_cal.acdb:system/etc/acdbdata/QRD/QRD_Speaker_cal.acdb \
+    $(LOCAL_PATH)/audio/acdb/workspaceFile.qwsp:system/etc/acdbdata/QRD/workspaceFile.qwsp \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths_qrd_skui.xml:system/etc/mixer_paths_qrd_skui.xml
 
 # Browser
