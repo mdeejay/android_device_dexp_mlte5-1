29b311669ede700f7f5776e959bcb49240d392cc
mdeejay <mdjrussia@gmail.com>
2017-08-29 19:01:53 +0300

    squashed commit: pins, launcher, zram
    Change-Id: Icfbc03220d60959297d9136b204ac7465284c50b

--- device.mk
+++ device.mk
@@ -59,9 +59,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths_qrd_skui.xml:system/etc/mixer_paths_qrd_skui.xml
 
-# Browser
+# Pixellauncher
 PRODUCT_PACKAGES += \
-    Gello
+    PixelLauncher
 
 # Camera
 PRODUCT_PACKAGES += \
