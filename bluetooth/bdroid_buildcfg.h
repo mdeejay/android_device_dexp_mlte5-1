8eb8deb0993e6eb19834608ecdef21d2cb7a8156
mdeejay <mdjrussia@gmail.com>
2017-08-30 03:42:06 +0300

    updates
    Change-Id: I7f0f509165cbf6529b451d1dda507f9cca4e25f0

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Dexp Ixion M 5 LTE"
+#define BTM_DEF_LOCAL_NAME   "Dexp Ixion M LTE 5"
 #define BLUETOOTH_QTI_SW TRUE
 #define BLE_VND_INCLUDED   TRUE
 #endif
