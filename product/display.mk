792806fca68793505003eee0bcda1c9859ddc55a
root <root@mdeejay.ru>
2017-09-07 07:27:05 +0300

    derp
    Change-Id: Id340093584f1820f3ec70b6b1cbb83f1ebee2784

--- product/display.mk
+++ product/display.mk
@@ -15,9 +15,9 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 #HWUI Props
 PRODUCT_PROPERTY_OVERRIDES += \
-dalvik.vm.heapgrowthlimit=128m
-dalvik.vm.heapminfree=6m
-dalvik.vm.heapstartsize=14m
-dalvik.vm.heapsize=256m
-dalvik.vm.heaptargetutilization=0.75
+dalvik.vm.heapgrowthlimit=128m \
+dalvik.vm.heapminfree=6m \
+dalvik.vm.heapstartsize=14m \
+dalvik.vm.heapsize=256m \
+dalvik.vm.heaptargetutilization=0.75 \
 dalvik.vm.heapmaxfree=8m
