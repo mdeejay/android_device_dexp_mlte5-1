d816bbb983384db5445cb47011f6cec5c02b9699
root <root@mdeejay.ru>
2017-09-09 15:48:10 +0300

    Replace vendor/cm with vendor/lineage

--- lineage.mk
+++ lineage.mk
@@ -15,7 +15,7 @@
 $(call inherit-product, device/DEXP/MLTE5/full_MLTE5.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
