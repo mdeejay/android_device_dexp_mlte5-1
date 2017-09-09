a72d573147ef4e455c8a4fa337d403ec90c6f34f
root <root@mdeejay.ru>
2017-09-09 16:12:39 +0300

    Set sepolicy to permissive

--- board/kernel.mk
+++ board/kernel.mk
@@ -3,9 +3,7 @@ ENABLE_CPUSETS := true
 TARGET_KERNEL_CONFIG := lineageos_MLTE5_defconfig
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
-BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 phy-msm-usb.floated_charger_enable=1
+BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 phy-msm-usb.floated_charger_enable=1 androidboot.selinux=permissive
 TARGET_KERNEL_ARCH := arm
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
-KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 TARGET_KERNEL_HEADER_ARCH := arm
 TARGET_USES_UNCOMPRESSED_KERNEL := false
