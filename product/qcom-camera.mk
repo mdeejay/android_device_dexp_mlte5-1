938ff5ba13c742eabc2b49f5e57b0b2676daaedf
root <root@mdeejay.ru>
2017-09-09 16:14:03 +0300

    Add STLport library as prebuilt binary

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -6,10 +6,6 @@ PRODUCT_PACKAGES += \
     libmm-qcamera \
     Snap
 
-# Build libstlport for vendor blobs
-PRODUCT_PACKAGES += \
-    libstlport
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
