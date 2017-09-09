eed2936f76b744115ef08a9267dce4c8ffd8f4f5
root <root@mdeejay.ru>
2017-09-09 17:55:54 +0300

    change kernel path

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,9 +1,5 @@
 # Kernel
 ENABLE_CPUSETS := true
+TARGET_KERNEL_SOURCE := kernel/dexp/msm8916
 TARGET_KERNEL_CONFIG := lineageos_MLTE5_defconfig
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_RAMDISK_OFFSET     := 0x01000000
 BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 phy-msm-usb.floated_charger_enable=1 androidboot.selinux=permissive
-TARGET_KERNEL_ARCH := arm
-TARGET_KERNEL_HEADER_ARCH := arm
-TARGET_USES_UNCOMPRESSED_KERNEL := false
