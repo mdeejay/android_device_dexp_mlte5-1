6d25968d6c22ad8018ae9a633f652f3018b0ad7a
mdeejay <mdjrussia@gmail.com>
2017-09-14 11:39:56 +0200

    cleanups

--- product/display.mk
+++ product/display.mk
@@ -6,22 +6,35 @@ TARGET_SCREEN_WIDTH := 720
 PRODUCT_PACKAGES += \
     libjni_livedisplay
 
-# HWUI
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.text_large_cache_height=2048
-
 # Screen density
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=320
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.hwui.use_buffer_age=false
-
+    
+# AAPT configs
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
+# Provides overrides to configure the Dalvik heap for a 3G phone
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapgrowthlimit=128m \
     dalvik.vm.heapminfree=6m \
-    dalvik.vm.heapstartsize=14m
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+    dalvik.vm.heapstartsize=14m \
+    dalvik.vm.heapsize=256m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapmaxfree=8m
+
+# Provides overrides to configure the HWUI memory limits
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=88 \
+    ro.hwui.layer_cache_size=58 \
+    ro.hwui.path_cache_size=32 \
+    ro.hwui.shape_cache_size=4 \
+    ro.hwui.gradient_cache_size=2 \
+    ro.hwui.drop_shadow_cache_size=8 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.text_small_cache_width=2048 \
+    ro.hwui.text_small_cache_height=2048 \
+    ro.hwui.text_large_cache_width=4096 \
+    ro.hwui.text_large_cache_height=4096 \
+    debug.hwui.use_buffer_age=false
+
